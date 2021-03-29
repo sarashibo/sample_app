8d3686c0ea62fb2b2a5c904c4697438a00d9b410
sarashibo <44943317+sarashibo@users.noreply.github.com>
2021-03-30 01:24:23 +0900

    cognito

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from "react";
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from "@aws-amplify/ui-react";
+
+Amplify.configure(awsconfig);
 
 function App() {
+  const [currentUserName, setCurrentUserName] = React.useState("");
+  useEffect(() => {
+    const init = async () => {
+      const currentUser = await Auth.currentAuthenticatedUser();
+      setCurrentUserName(currentUser.username);
+    }
+    init()
+  }, []);
+
+  const signOut = async () => {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.log('error signing out: ', error);
+    }
+    document.location.reload();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          sample_app deploy completed.
+          {currentUserName} login
         </p>
+        <button onClick={signOut}>サインアウト</button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
